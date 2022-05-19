9611280360a83fb53153d5639d163d3c618337c1
shay <shayosullivan@hotmail.com>
2022-05-19 11:05:06 -0400

    things

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+//DEPENDENCIES
+
+// STATE
+
+//FUNCTIONS
+//run clock - moment $ set interval
+/**
+ * a function that updates the current time every second
+ *
+ */
+function runClock() {
+  console.log(" runs the clock");
+}
+
+//INTERACTIONS
+// user clicks add project
+
+//INITIALIZATION
